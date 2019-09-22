fa9319de8449a504e2e83eecedf7a3afbdc005ac
Nseghe Essiet <essietnseghe@gmail.com>
2019-09-22 15:06:59 +0100

    Fixed Database query for license numbers

--- ocr_app/license_owners/forms.py
+++ ocr_app/license_owners/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, RadioField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError
+from ocr_app.models import License_Owner
 
 
 class CustomerForm(FlaskForm):
@@ -10,7 +11,15 @@ class CustomerForm(FlaskForm):
     accounttype = RadioField('Account Type', choices=[('savings', 'Savings'), ('current', 'Current')])
     submit = SubmitField('Predict Loan Default')
 
+
 class LicensePlateForm(FlaskForm):
     plate_number = StringField('License Plate Number',
                            validators=[DataRequired(), Length(min=3, max=9)])
     submit = SubmitField('Search Database')
+
+    def validate_plate_number(self, plate_number):
+        license_owner = License_Owner.query.filter_by(license_number=plate_number.data).first()
+        if len(plate_number.data)>2 and len(plate_number.data)<10:
+            if not license_owner:
+                raise ValidationError('That License Number does not exist in our database. Please try again.')
+
--- ocr_app/license_owners/routes.py
+++ ocr_app/license_owners/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, redirect, url_for, Blueprint
+from flask import render_template, redirect, request, url_for, Blueprint
 from flask_login import login_required
 from ocr_app.license_owners.forms import LicensePlateForm
-from ocr_app.models import License_Owners
+from ocr_app.models import License_Owner
 
 license_owners = Blueprint('license_owners', __name__)
 
@@ -19,6 +19,14 @@ def license_plate_recognition():
 @login_required
 def license_owner():
 	# id = int(request.form["id"])
-	license_owner = License_Owners.query.get(1)
+	license_owner = License_Owner.query.get('AAA841LE')
 	# #present_customer.age = present_customer.age  # type: object
 	return render_template('license_owner.html', title='License Owner', license_owner=license_owner)
+
+
+@license_owners.route("/show_all_license_owners", methods=['GET', 'POST'])
+@login_required
+def show_all_license_owners():
+    page = request.args.get('page', 1, type=int)
+    all_license_owners = License_Owner.query.order_by(License_Owner.id.desc()).paginate(page=page, per_page=50)
+    return render_template('show_all_license_owners.html', title='All Customers', all_license_owners=all_license_owners)
