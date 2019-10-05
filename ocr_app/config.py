6e990849a34801158396f5c5bd431c2d814b75dd
Nseghe Essiet <essietnseghe@gmail.com>
2019-10-05 18:48:08 +0100

    Update config.py

--- ocr_app/config.py
+++ ocr_app/config.py
@@ -8,5 +8,5 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'noreply.algostacks@gmail.com'
-    MAIL_PASSWORD = '2569nserah'
+    MAIL_USERNAME = 
+    MAIL_PASSWORD = 
